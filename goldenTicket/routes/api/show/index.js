891d2755bb2f2ff793420f9aea7c132077386ce1
HwangJaeSuk <jaesukhwang95@gmail.com>
2019-06-27 22:45:14 +0900

    content router

--- goldenTicket/routes/api/show/index.js
+++ goldenTicket/routes/api/show/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/content', require('./content'));
 router.use('/like', require('./like'));
 router.use('/schedule', require('./schedule'));
 router.use('/', require('./show'));
