f229ca8e9cb4b977bbc3fb55c1f839628462fba0
lumiere <heesung6701@naver.com>
2019-06-28 23:15:24 +0900

    remove semicolon

--- goldenTicket/routes/api/lottery/lottery.js
+++ goldenTicket/routes/api/lottery/lottery.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 // 티켓 응모하기
 router.post('/', async (req, res) => {
+})
 
-});
-
-module.exports = router;
+module.exports = router
