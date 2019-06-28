5a1983267e98dc81eda0d8944aad3501c530d0b7
heesung6701 <heesung6701@naver.com>
2019-06-28 22:00:03 +0900

    Revert "Feature/routing"

--- goldenTicket/routes/api/auth/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.use('/signin', require('./signin'));
-router.use('/signup', require('./signup'));
-
-module.exports = router;
--- goldenTicket/routes/api/show/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/content', require('./content'));
-router.use('/like', require('./like'));
-router.use('/schedule', require('./schedule'));
-router.use('/', require('./show'));
-
-module.exports = router;
