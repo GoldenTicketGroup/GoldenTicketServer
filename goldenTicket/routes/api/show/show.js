916286754c7da4472183352ab71740844c923239
unknown <cmk5378@naver.com>
2019-07-01 16:49:31 +0900

    show apply modified

--- goldenTicket/routes/api/show/show.js
+++ goldenTicket/routes/api/show/show.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const showModule = require('../../../models/show')
+const upload = require('../../../config/multer')
 
 //공연 리스트 조회
 router.get('/', async(req, res) => {
@@ -12,8 +14,26 @@ router.get('/:id', async(req, res) => {
 });
 
 //공연 등록
-router.post('/', async(req, res) => {
-    res.status(200).send("test1");
+router.post('/', upload.single('imageUrl'), async(req, res) => {
+    //res.status(200).send("test1");
+    const imageUrl = req.file.location
+    const name = req.body.name
+    const originalPrice = req.body.originalPrice
+    const discountPrice = req.body.discountPrice
+    const location = req.body.location
+    const accountHolder = req.body.accountHolder
+    const accountNumber = req.body.accountNumber
+    const showInfo = {
+        imageUrl,
+        name,
+        originalPrice,
+        discountPrice,
+        location,
+        accountHolder,
+        accountNumber
+    }
+    const result = await showModule.apply(showInfo)
+    res.status(200).send(result)
 });
 
 //공연 수정
