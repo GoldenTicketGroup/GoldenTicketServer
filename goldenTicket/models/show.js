eec4a3e2e46dd60e3c44caaad40f34317c745daf
ChoiMinKyung <cmk5378@gmail.com>
2019-07-02 19:04:32 +0900

    models/show modified

--- goldenTicket/models/show.js
+++ goldenTicket/models/show.js
@@ -28,7 +28,7 @@ const showModule = {
         if (!result) {
             return new errorMsg(true, Utils.successFalse(CODE.DB_ERROR, MSG.FAIL_CREATED_X(WORD)))
         }
-        if (result.isError == true && typeof(result.jsonData) == 'string') { //필요한 값이 없습니다. 일때
+        if (result.isError == true && typeof(result.jsonData) == 'string') {
             return new errorMsg(true, Utils.successFalse(CODE.BAD_REQUEST, result.jsonData))
         }
         if (result.isError == true) {
