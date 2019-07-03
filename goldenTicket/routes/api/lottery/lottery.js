7ed813d782a7ddbd9c734992885071f6a2f6ec0b
ChoiMinKyung <cmk5378@gmail.com>
2019-07-03 18:35:15 +0900

    lottery frame

--- goldenTicket/routes/api/lottery/lottery.js
+++ goldenTicket/routes/api/lottery/lottery.js
@@ -6,4 +6,29 @@ router.post('/', async (req, res) => {
     res.status(200).send("lottery test1")
 })
 
+// 티켓 수정
+router.put('/', async (req, res) => {
+    res.status(200).send("lottery test2")
+})
+
+// 티켓 응모 상세 조회
+router.get('/:id', async (req, res) => {
+    res.status(200).send("lottery test3")
+})
+
+// 티켓 응모 리스트 조회
+router.get('/', async (req, res) => {
+    res.status(200).send("lottery test4")
+})
+
+//티켓 응모 삭제
+router.delete('/:id', async (req, res) => {
+    res.status(200).send("lottery test5")
+})
+
+// 당첨 티켓 추첨
+// router.select('/:name', async (req, res) => {
+//     res.status(200).send("lottery test6")
+// })
+
 module.exports = router
