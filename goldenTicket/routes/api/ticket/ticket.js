2f4538cc102adec288d5a7b71db8beb703188807
ChoiMinKyung <cmk5378@gmail.com>
2019-07-04 14:14:23 +0900

    ticket api frame

--- goldenTicket/routes/api/ticket/ticket.js
+++ goldenTicket/routes/api/ticket/ticket.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-// 당첨 티켓 관련
+// 당첨 티켓 등록
 router.post('/', async (req, res) => {
     res.status(200).send("test1")
 });
 
+// 당첨 티켓 수정
+router.put('/', async (req, res) => {
+    res.status(200).send("test2")
+});
+
+// 당첨 티켓 상세 조회
+router.get('/:id', async (req, res) => {
+    res.status(200).send("test3")
+});
+
+// 당첨 티켓 전체 조회
+router.get('/', async (req, res) => {
+    res.status(200).send("test4")
+});
+
+// 당첨 티켓 삭제 부분은 관리자가 직접 삭제
 module.exports = router;
