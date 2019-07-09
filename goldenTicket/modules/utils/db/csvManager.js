e9d86a0247b8a158d9351542c8aaf2676a48bfd8
lumiere <heesung6701@naver.com>
2019-07-09 21:14:08 +0900

    cron 스케쥴러 구현

--- goldenTicket/modules/utils/db/csvManager.js
+++ goldenTicket/modules/utils/db/csvManager.js
@@ -1,4 +1,7 @@
 const responseMessage = require('../../utils/rest/responseMessage')
+responseMessage.FAIL_CSV_READ = "CSV 읽기 실패"
+responseMessage.FAIL_
+const errorMsg = require('../common/errorUtils')
 const fs = require('fs')
 const json2csv = require('json2csv')
 const csv = require('csvtojson')
@@ -24,11 +27,25 @@ const csvManager = {
             })
         })
     },
+    csvWriteSync: (fileName, jsonArray) => {
+        console.log(JSON.stringify(jsonArray))
+        const resultCsv = json2csv.parse(jsonArray)
+        console.log(resultCsv)
+        try{
+            fs.writeFileSync(`${csv_url}${fileName}`, resultCsv)
+        }catch(err){
+            console.log(`file save(${csv_url}${fileName}) err: ${result}`)
+            return new errorMsg(true, result)
+        }
+        console.log(`All of complete(${csv_url}${fileName})`)
+        return true
+        
+    },
     csvRead: (fileName) => {
         return new Promise((resolve, reject) => {
             try {
                 if (fs.existsSync(`${csv_url}${fileName}`) == false) {
-                    throw Error(responseMEssage.FAIL_CSV_READ)
+                    throw Error(responseMessage.FAIL_CSV_READ)
                 }
             } catch (err) {
                 console.log(`file(${csv_url}${fileName}) not exist`)
@@ -38,7 +55,7 @@ const csvManager = {
             csv().fromFile(`${csv_url}${fileName}`).then((jsonArr) => {
                 if (!jsonArr) {
                     console.log(`file read(${csv_url}${fileName}) err: ${err}`)
-                    reject(responseMEssage.FAIL_CSV_READ)
+                    reject(responseMessage.FAIL_CSV_READ)
                     return
                 }
                 for (const idx in jsonArr) {
@@ -49,9 +66,27 @@ const csvManager = {
                 resolve(jsonArr)
             }, (err) => {
                 console.log(`err with readCSV: ${err}`)
-                reject(responseMEssage.FAIL_CSV_READ)
+                reject(responseMessage.FAIL_CSV_READ)
             })
         })
+    },
+    csvReadSync: async (fileName) => {
+        try {
+            if (fs.existsSync(`${csv_url}${fileName}`) == false) {
+                return new errorMsg(true, responseMessage.FAIL_CSV_READ)
+            }
+        } catch (err) {
+            console.log(`file(${csv_url}${fileName}) not exist`)
+            return new errorMsg(true, err)
+        }
+        const result = await csv().fromFile(`${csv_url}${fileName}`)
+
+        if(!result) {
+            console.log(`file read(${csv_url}${fileName}) err: ${err}`)
+            return new errorMsg(true, result)
+        }
+        console.log(`All of complete(${csv_url}${fileName})!`)
+        return result
     },
     csvReadSingle: (fileName, idx) => {
         return new Promise(async (resolve, reject) => {
@@ -70,6 +105,20 @@ const csvManager = {
             resolve(jsonData)
         })
     },
+    csvReadSingleSync: async (fileName, idx) => {
+        const jsonArr = await csvManager.csvRead(fileName)
+        let jsonData = null
+        for (const i in jsonArr) {
+            if (jsonArr[i].idx == idx) {
+                jsonData = jsonArr[i]
+                break
+            }
+        }
+        if (jsonData == null) {
+            return new errorMsg(true, responseMessage.NO_INDEX)
+        }
+        return jsonData
+    },
     csvAdd: async (fileName, jsonData) => {
         const jsonArr = await csvManager.csvRead(fileName)
         let prevIdx = 0
@@ -80,19 +129,21 @@ const csvManager = {
         jsonArr.push(jsonData)
         return csvManager.csvWrite(fileName, jsonArr)
     },
-    logWrite: async (fileName, date, stringData) => {    
+    logWrite: async (fileName, stringData, date) => {
+        if(date == null) date = new Date()
         const nowMoment = moment(date)
         const dateStr = nowMoment.format("YYYY-MM-DD HH:MM:SS")
         fs.appendFile(`${log_url}${fileName}`, `[${dateStr}] ${stringData}\n`, (err) => {
-            if(err) new Error(err)
+            if (err) new Error(err)
             console.log(`saved log ${stringData}`)
         })
         return
     },
     CSV_READY_TO_SCHEDULE_LIST: "ready2scheduleList.csv",
-    CSV_LOTTERY_LIST_WITH_SCHEDULE_CACHE: (idx)=> `lotterySaveCache_${idx}`,
+    CSV_LOTTERY_LIST_WITH_SCHEDULE_CACHE: (idx) => `lotterySaveCache_${idx}.csv`,
     LOG_TO_UPDATE_AVAILABLE: "log2updateAvailable.csv",
     LOG_DURING_SAVE_CACHE: "logDuringSaveCache.csv",
+    LOG_DURING_CHOOSE_WIN: "logDuringChooseWin.csv",
 }
 
 module.exports = csvManager
--- goldenTicket/modules/utils/scheduler/scheduler.js
+++ goldenTicket/modules/utils/scheduler/scheduler.js
@@ -1,9 +1,89 @@
 const moment = require('moment')
 const cron = require('node-cron')
 const csvManager = require('../db/csvManager')
-const Lottery = require('../../../models/lottery')
-const Schedule = require('../../../models/schedule')
+const sqlManager = require('../db/sqlManager')
 const db = require('../db/pool')
+const Lottery = {
+    update: async (setJson, whereJson, sqlFunc) => {
+        const TABLE_NAME = sqlManager.TABLE_LOTTERY
+        const func = sqlFunc || db.queryParam_Parse
+        const result = await sqlManager.db_update(func, TABLE_NAME, setJson, whereJson)
+        if (!result) {
+            return new errorMsg(true, MSG.FAIL_UPDATED_X(WORD))
+        }
+        return result
+    },
+    selectAll: async (whereJson, opts, sqlFunc) => {
+        const TABLE_NAME = sqlManager.TABLE_LOTTERY
+        const func = sqlFunc || db.queryParam_Parse
+        const result = await sqlManager.db_select(func, TABLE_NAME, whereJson, opts)
+        if (result.length == undefined) {
+            return new errorMsg(true, MSG.FAIL_READ_X_ALL(WORD))
+        }
+        return result
+    }
+}
+const Schedule = {
+    apply: async (jsonData, sqlFunc) => {
+        const WORD = "스케쥴"
+        const TABLE_NAME = sqlManager.TABLE_SCHEDULE
+        const func = sqlFunc || db.queryParam_Parse
+        const result = await sqlManager.db_insert(func, TABLE_NAME, jsonData)
+        if (!result) {
+            return new errorMsg(true, MSG.FAIL_CREATED_X(WORD))
+        }
+        return result
+    },
+    select: async (whereJson, opts, sqlFunc) => {
+        const WORD = "스케쥴"
+        const TABLE_NAME = sqlManager.TABLE_SCHEDULE
+        const func = sqlFunc || db.queryParam_Parse
+        const result = await sqlManager.db_select(func, TABLE_NAME, whereJson, opts)
+        if (result.length == undefined) {
+            return new errorMsg(true,MSG.FAIL_READ_X(WORD))
+        }
+        if (result.length == 0) {
+            return new errorMsg(true, MSG.NO_X(WORD))
+        }
+        return result[0]
+    },
+    getList: async (whereJson, opts, sqlFunc) => {
+        const WORD = "스케쥴"
+        const TABLE_NAME = sqlManager.TABLE_SCHEDULE
+        const func = sqlFunc || db.queryParam_Parse
+        const result = await sqlManager.db_select(func, TABLE_NAME, whereJson, opts)
+        if (result.length == undefined) {
+            return new errorMsg(true,  MSG.FAIL_READ_X_ALL(WORD))
+        }
+        return result
+    },
+    update: async (setJson, whereJson, sqlFunc) => {
+        const WORD = "스케쥴"
+        const TABLE_NAME = sqlManager.TABLE_SCHEDULE
+        const func = sqlFunc || db.queryParam_Parse
+        const result = await sqlManager.db_update(func, TABLE_NAME, setJson, whereJson)
+        if (!result) {
+            return new errorMsg(true, MSG.FAIL_UPDATED_X(WORD))
+        }
+        if (result.affectedRows == 0) {
+            return new errorMsg(true,  MSG.NO_X(WORD))
+        }
+        return result
+    },
+    remove: async (whereJson, sqlFunc) => {
+        const WORD = "스케쥴"
+        const TABLE_NAME = sqlManager.TABLE_SCHEDULE
+        const func = sqlFunc || db.queryParam_Parse
+        const result = await sqlManager.db_delete(func, TABLE_NAME, whereJson)
+        if (!result) {
+            return new errorMsg(true, CODE.DB_ERROR, MSG.FAIL_REMOVED_X(WORD))
+        }
+        if (result.affectedRows == 0) {
+            return new errorMsg(true, CODE.DB_ERROR, MSG.NO_X(WORD))
+        }
+        return result
+    }
+}
 
 const cronList = []
 let waitCronList = []
@@ -71,51 +151,58 @@ const taskReady2Choose = async (now) => {
         const csvScheduleList = scheduleResult.map((it)=> convertSchedule4csv(it))
         await csvManager.csvWrite(csvManager.CSV_READY_TO_SCHEDULE_LIST, csvScheduleList)
 
+        for(const it in waitCronList){
+            it.end()
+        }
         waitCronList = []
         for(const schedule of csvScheduleList) {
             const scheduleIdx = schedule.scheduleIdx
-            const waitTask = new CronWithIndex(scheduleIdx)
             const tmpDate = new Date(`${schedule.date}T${schedule.startTime}Z`)
             const hour = tmpDate.getUTCHours()
             const minute = tmpDate.getUTCMinutes()
-            const condition = `${minute} ${hour} * * *`
-            console.log(condition)
-            waitTask.start(condition, taskSaveCache)
-            waitCronList.push(waitTask)
+
+            const cacheTask = new CronWithIndex(scheduleIdx)
+            const conditionForCache = `${minute} ${hour - 3} * * *`
+            console.log(conditionForCache)
+            cacheTask.start(conditionForCache, taskSaveCache)
+            
+            const chooseTask = new CronWithIndex(scheduleIdx)
+            const conditionForChoose = `${minute} ${hour - 2} * * *`
+            console.log(conditionForChoose)
+            cacheTask.start(conditionForChoose, taskChooseWin)
+
+            waitCronList.push(cacheTask)
+            waitCronList.push(chooseTask)
         }
     })
     if(transaction.isError) {
         console.log(transaction.jsonData)
-        csvManager.logWrite(csvManager.LOG_TO_UPDATE_AVAILABLE, now, '매일 10시에 스케쥴 리스트 csv로 저장하기 실패')
+        csvManager.logWrite(csvManager.LOG_TO_UPDATE_AVAILABLE, '매일 10시에 스케쥴 리스트 csv로 저장하기 실패', now)
         setTimeout(() => {
             taskReady2Choose(new Date())
         },60 * 10)
         return
     }
-    csvManager.logWrite(csvManager.LOG_TO_UPDATE_AVAILABLE,now, '매일 10시에 스케쥴 리스트 csv로 저장하기 성공')
+    csvManager.logWrite(csvManager.LOG_TO_UPDATE_AVAILABLE, '매일 10시에 스케쥴 리스트 csv로 저장하기 성공', now)
 }
 const taskSaveCache = async (scheduleIdx) => {
     console.log(`Database에서 추첨을 해야할 정보를 미리 파일로 가져온다.`)
     console.log(scheduleIdx)
-    const schedule = await csvManager.csvReadSingle(csvManager.CSV_READY_TO_SCHEDULE_LIST, scheduleIdx)
-    console.log(schedule)
     
     const whereJson = {scheduleIdx: scheduleIdx}
     const opts = {}
     const lotteryResult = await Lottery.selectAll(whereJson, opts)
     if (lotteryResult.isError == true) {
-        console.log('error' + resultList.jsonData)
+        console.log(`error ${lotteryResult.jsonData}`)
         await csvManager.logWrite(csvManager.LOG_DURING_SAVE_CACHE, 'fail to read lotteryList')
         setTimeout(() => {
             taskSaveCache(scheduleIdx)
-        },60 * 10)
+        },60 * 10 * 60)
         return
     }
-    const csvJson = {
-        showIdx: showIdx,
-        lotteryList: lotteryResult.map((it) => convertLottery4csv(it))
-    }
-    const result  = await csvManager.csvWrite(csvManager.CSV_LOTTERY_LIST_WITH_SCHEDULE_CACHE(scheduleIdx), csvJson)
+    const csvJson = lotteryResult.map((it) => convertLottery4csv(it))
+
+    const result  = await csvManager.csvWriteSync(csvManager.CSV_LOTTERY_LIST_WITH_SCHEDULE_CACHE(scheduleIdx), csvJson)
     console.log(result)
     if (!result) {
         console.log('error on save cache lottery')
@@ -126,44 +213,79 @@ const taskSaveCache = async (scheduleIdx) => {
         return
     }
 }
-const taskChoseWin = () => {
-    console.log("1시간 마다 실행")
-    console.log(moment().format('YYYY-MM-DD HH:mm:ss'))
-    console.log(`한시간 마다 push보내야할 로터리 확인 해야한다.`)
-    // 로터리 리스트 가져오기 (from file)
-    // random 알고리즘
-    // db update(트랜잭션 처리) //이부분에서 메모리 초과 일어날 것같음. 추후에 update를 하기전에 csv로 저장하고 db로 업데이트
-    // fcm 메세지 보내기
+const taskChooseWin = async (scheduleIdx) => {
+    console.log(`당첨 로터리 티켓 고르기`)
+    
+    // 1. 로터리 리스트 가져오기 (from file)
+    let cacheLotteryList = await csvManager.csvReadSync(csvManager.CSV_LOTTERY_LIST_WITH_SCHEDULE_CACHE(scheduleIdx))
+    if(cacheLotteryList.isError == true){        
+        cacheLotteryList = await Lottery.selectAll({scheduleIdx: scheduleIdx}, {})
+        if(cacheLotteryList.isError == true){
+            await csvManager.logWrite(csvManager.LOG_DURING_CHOOSE_WIN, `로터리 당첨 선발중에 에러가 발생했습니다...${cacheLotteryList.jsonData}`)
+            return
+        }
+    }
+    const resultSchedule = await Schedule.select({scheduleIdx: scheduleIdx})
+    if(resultSchedule.isError == true){
+        await csvManager.logWrite(csvManager.LOG_DURING_CHOOSE_WIN, `로터리 당첨 선발중에 에러가 발생했습니다...${resultSchedule.jsonData}`)
+        return
+    }
+    if(resultSchedule.length == 0){
+        await csvManager.logWrite(csvManager.LOG_DURING_CHOOSE_WIN, "로터리 당첨 선발중에 에러가 발생했습니다... [해당되는 스케쥴이 없습니다.]")
+        return
+    }
+    console.log(resultSchedule)
+    if(resultSchedule.done == 1 ){
+        await csvManager.logWrite(csvManager.LOG_DURING_CHOOSE_WIN, "로터리 당첨 선발중에 에러가 발생했습니다... [이미 당첨 프로세스를 거친 스케쥴입니다.]")
+        return
+    }
+    
+    // 2. random 알고리즘
+    const randomIdx = parseInt(Math.random() * 1000) % cacheLotteryList.length
+    const winLottery = cacheLotteryList[randomIdx]
+    if(!winLottery.lotteryIdx){
+        await csvManager.logWrite(csvManager.LOG_DURING_CHOOSE_WIN, "로터리 당첨 선발중에 에러가 발생했습니다...")
+        return
+    }
+    csvManager.logWrite(csvManager.LOG_DURING_CHOOSE_WIN, `scheduleIdx:${scheduleIdx}/ winLotteryIdx is ${winLottery.lotteryIdx}`)
+
+    
+    // db update(트랜잭션 처리)
+    const transaction = await db.Transaction(async (connection) => {
+        const queryStreamFunc = (query, value) => {
+            return connection.queryStream(query, value)
+        }
+        const resultUpdate = await Schedule.update({done: 1}, {scheduleIdx: scheduleIdx}, queryStreamFunc)
+        const resultLottery = await Lottery.update({state: 1}, {lotteryIdx: winLottery.lotteryIdx})
+    })
+    if(!transaction){
+        return new errorMsg(false,  Utils.successFalse(CODE.DB_ERROR, MSG.FAIL_UPDATED_X(WORD)))
+    }
+    console.log(transaction)
+    //FCM을 보낸다
+
     // csv 정보를 clear한다.
+    return transaction
 }
 const scheduler = {
     startCron: () => {
-        const taskWhen10oClock = cron.schedule('0 10 * * *', () => {        
+        console.log("start Cron")
+        const taskWhen10oClock = cron.schedule('*/1 * * * *', () => {        
+        // const taskWhen10oClock = cron.schedule('0 10 * * *', () => {        
             console.log("10시 에 실행")
             console.log(moment().format('YYYY-MM-DD HH:mm:ss'))
             taskReady2Choose()
         })
-        // 10시 30분 부터 22시 30분 까지 1시간 마다 실행
-        // csv로 미리 db에서 파일로 옮겨놓음 (캐시 역할)
-        const taskEvery30Minute = cron.schedule('30 10-22/1 * * *', () => {
-            console.log("1시간 마다(30분에) 실행")
-            console.log(moment().format('YYYY-MM-DD HH:mm:ss'))
-            taskSaveCache()
-        })
-        // 10시부터 22시까지 1시간 마다실행
-        const task2 = cron.schedule('0 10-22/1 * * *', () => {
-            taskChoseWin()
-        })
         cronList.push(taskWhen10oClock)
-        cronList.push(task2)
-        cronList.push(taskEvery30Minute)
     },
     stopCron: () => {
+        console.log("stop Cron")
         for (const schedule in cronList) {
             schedule.stop()
         }
     },
     endCron: () => {
+        console.log("end Cron")
         for (const schedule in cronList) {
             schedule.destroy()
         }
@@ -181,12 +303,16 @@ const test_taskReady2Choose_reset = async () => {
     await Schedule.update({drawAvailable: 0}, {date: dateStr})
 }
 const test_taskSavaCache = async () => {
-    await taskSaveCache(30)
+    await taskSaveCache(60)
+}
+const test_taskChooseWin = async () => {
+    await taskChooseWin(60)
 }
 const test_module = async () => {
     // scheduler.startCron()
     // test_taskReady2Choose_reset()
-    await test_taskSavaCache()
+    // await test_taskSavaCache()
     // test_taskReady2Choose()
+    // test_taskChooseWin()
 }
-test_module()
+// test_module()
