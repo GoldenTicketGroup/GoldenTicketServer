0de7e062e90aa78616d2883671255179766a2528
lumiere <heesung6701@naver.com>
2019-07-11 20:50:49 +0900

    fix cron error

--- goldenTicket/modules/utils/scheduler/scheduler.js
+++ goldenTicket/modules/utils/scheduler/scheduler.js
@@ -3,22 +3,26 @@ const cron = require('node-cron')
 const csvManager = require('../db/csvManager')
 const sqlManager = require('../db/sqlManager')
 const db = require('../db/pool')
+const errorMsg = require('../common/errorUtils')
+const responseMessage = require('../rest/responseMessage')
 const Lottery = {
     update: async (setJson, whereJson, sqlFunc) => {
         const TABLE_NAME = sqlManager.TABLE_LOTTERY
+        const WORD = '로터리'
         const func = sqlFunc || db.queryParam_Parse
         const result = await sqlManager.db_update(func, TABLE_NAME, setJson, whereJson)
         if (!result) {
-            return new errorMsg(true, MSG.FAIL_UPDATED_X(WORD))
+            return new errorMsg(true, responseMessage.FAIL_UPDATED_X(WORD))
         }
         return result
     },
     selectAll: async (whereJson, opts, sqlFunc) => {
         const TABLE_NAME = sqlManager.TABLE_LOTTERY
+        const WORD = '로터리'
         const func = sqlFunc || db.queryParam_Parse
         const result = await sqlManager.db_select(func, TABLE_NAME, whereJson, opts)
         if (result.length == undefined) {
-            return new errorMsg(true, MSG.FAIL_READ_X_ALL(WORD))
+            return new errorMsg(true, responseMessage.FAIL_READ_X_ALL(WORD))
         }
         return result
     }
@@ -30,7 +34,7 @@ const Schedule = {
         const func = sqlFunc || db.queryParam_Parse
         const result = await sqlManager.db_insert(func, TABLE_NAME, jsonData)
         if (!result) {
-            return new errorMsg(true, MSG.FAIL_CREATED_X(WORD))
+            return new errorMsg(true, responseMessage.FAIL_CREATED_X(WORD))
         }
         return result
     },
@@ -40,10 +44,10 @@ const Schedule = {
         const func = sqlFunc || db.queryParam_Parse
         const result = await sqlManager.db_select(func, TABLE_NAME, whereJson, opts)
         if (result.length == undefined) {
-            return new errorMsg(true,MSG.FAIL_READ_X(WORD))
+            return new errorMsg(true,responseMessage.FAIL_READ_X(WORD))
         }
         if (result.length == 0) {
-            return new errorMsg(true, MSG.NO_X(WORD))
+            return new errorMsg(true, responseMessage.NO_X(WORD))
         }
         return result[0]
     },
@@ -53,7 +57,7 @@ const Schedule = {
         const func = sqlFunc || db.queryParam_Parse
         const result = await sqlManager.db_select(func, TABLE_NAME, whereJson, opts)
         if (result.length == undefined) {
-            return new errorMsg(true,  MSG.FAIL_READ_X_ALL(WORD))
+            return new errorMsg(true,  responseMessage.FAIL_READ_X_ALL(WORD))
         }
         return result
     },
@@ -63,10 +67,10 @@ const Schedule = {
         const func = sqlFunc || db.queryParam_Parse
         const result = await sqlManager.db_update(func, TABLE_NAME, setJson, whereJson)
         if (!result) {
-            return new errorMsg(true, MSG.FAIL_UPDATED_X(WORD))
+            return new errorMsg(true, responseMessage.FAIL_UPDATED_X(WORD))
         }
         if (result.affectedRows == 0) {
-            return new errorMsg(true,  MSG.NO_X(WORD))
+            return new errorMsg(true,  responseMessage.NO_X(WORD))
         }
         return result
     },
@@ -76,10 +80,10 @@ const Schedule = {
         const func = sqlFunc || db.queryParam_Parse
         const result = await sqlManager.db_delete(func, TABLE_NAME, whereJson)
         if (!result) {
-            return new errorMsg(true, CODE.DB_ERROR, MSG.FAIL_REMOVED_X(WORD))
+            return new errorMsg(true, CODE.DB_ERROR, responseMessage.FAIL_REMOVED_X(WORD))
         }
         if (result.affectedRows == 0) {
-            return new errorMsg(true, CODE.DB_ERROR, MSG.NO_X(WORD))
+            return new errorMsg(true, CODE.DB_ERROR, responseMessage.NO_X(WORD))
         }
         return result
     }
@@ -108,9 +112,9 @@ class CronWithIndex {
         if(!this.currentTask) return
         this.currentTask.stop()
     }
-    end() {
+    destroy() {
         if(!this.currentTask) return
-        this.currentTask.end()
+        this.currentTask.destroy()
     }
 }
 const convertSchedule4csv = (it) => {
@@ -151,8 +155,8 @@ const taskReady2Choose = async (now) => {
         const csvScheduleList = scheduleResult.map((it)=> convertSchedule4csv(it))
         await csvManager.csvWrite(csvManager.CSV_READY_TO_SCHEDULE_LIST, csvScheduleList)
 
-        for(const it in waitCronList){
-            it.end()
+        for(const it of waitCronList){
+            it.destroy()
         }
         waitCronList = []
         for(const schedule of csvScheduleList) {
@@ -259,7 +263,7 @@ const taskChooseWin = async (scheduleIdx) => {
         const resultLottery = await Lottery.update({state: 1}, {lotteryIdx: winLottery.lotteryIdx})
     })
     if(!transaction){
-        return new errorMsg(false,  Utils.successFalse(CODE.DB_ERROR, MSG.FAIL_UPDATED_X(WORD)))
+        return new errorMsg(false,  Utils.successFalse(CODE.DB_ERROR, responseMessage.FAIL_UPDATED_X(WORD)))
     }
     console.log(transaction)
     //FCM을 보낸다
@@ -270,8 +274,8 @@ const taskChooseWin = async (scheduleIdx) => {
 const scheduler = {
     startCron: () => {
         console.log("start Cron")
-        const taskWhen10oClock = cron.schedule('*/1 * * * *', () => {        
-        // const taskWhen10oClock = cron.schedule('0 10 * * *', () => {        
+        // const taskWhen10oClock = cron.schedule('*/1 * * * *', () => {        
+        const taskWhen10oClock = cron.schedule('0 10 * * *', () => {        
             console.log("10시 에 실행")
             console.log(moment().format('YYYY-MM-DD HH:mm:ss'))
             taskReady2Choose()
@@ -293,11 +297,11 @@ const scheduler = {
 }
 module.exports = scheduler
 const test_taskReady2Choose = async () => {
-    const now = new Date('2019-07-08')
+    const now = new Date('2019-07-11')
     await taskReady2Choose(now)
 }
 const test_taskReady2Choose_reset = async () => {   
-    const now = new Date('2019-07-08')
+    const now = new Date('2019-07-11')
     const nowMoment = moment(now)
     const dateStr = nowMoment.format("YYYY-MM-DD")
     await Schedule.update({drawAvailable: 0}, {date: dateStr})
@@ -311,8 +315,10 @@ const test_taskChooseWin = async () => {
 const test_module = async () => {
     // scheduler.startCron()
     // test_taskReady2Choose_reset()
+    // test_taskReady2Choose()
+    // test_taskReady2Choose()
     // await test_taskSavaCache()
     // test_taskReady2Choose()
     // test_taskChooseWin()
 }
-// test_module()
+//test_module()
