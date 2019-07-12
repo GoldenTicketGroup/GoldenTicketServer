8e7312a81c37cd7e13ea3afbf9d58c83851782d5
lumiere <heesung6701@naver.com>
2019-07-13 01:51:48 +0900

    자동 cron을 추가 안한다.

--- goldenTicket/routes/index.js
+++ goldenTicket/routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const scheduler = require('../modules/utils/scheduler/scheduler')
-scheduler.startCron()
+// const scheduler = require('../modules/utils/scheduler/scheduler')
+// scheduler.startCron()
 
 router.use('/', require('./api'))
+router.use('/test', require('./test'))
 
 module.exports = router
